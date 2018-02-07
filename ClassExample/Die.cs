f1df97149c30eda8dcb2b993f81e984505ddcf27
Roadhog31 <RoadHog31@users.noreply.github.com>
2018-02-07 21:18:13 +0000

    Add project files.

---
+++ ClassExample/Die.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExample
+{
+    class Die
+    {
+        private int face;
+
+        // Static Class Random
+        Random rand = new Random();
+
+        // default constructor
+        public Die()
+        {
+            face = rand.Next(6) + 1;
+        }
+
+        public int Face
+        {
+            get { return face; }
+            set { face = value; }
+        }
+
+        public int roll()
+        {
+            face = rand.Next(6) + 1;
+            return face;
+        }
+
+        public override string ToString()
+        {
+            return "Face value is: " + face;
+        }
+    }
+}
---
+++ ClassExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassExample
+{
+    class Program
+    {
+        static void Main(string[] args )
+        {
+            Die die1 = new Die();
+            Console.Out.WriteLine(die1);
+            
+            for (int i = 0; i < 10; i++)
+            {            
+              die1.roll();
+              Console.WriteLine(die1); 
+            }
+            Console.ReadLine();
+        }
+
+
+    }
+}
+
